d0d9a69f4830537de68b731783ddf4a2ddea7ef3
Jia Sheng Tay <tayjiasheng@Jias-MacBook-Pro.local>
2025-07-15 16:05:06 +0800

    fix web polling bug

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from flask import Flask, request
 import os
 import telebot
 
+TOKEN = os.getenv("BOT_TOKEN")
+WEBHOOK_PATH = f"/{TOKEN}"
+WEBHOOK_URL = f"https://{os.getenv('RENDER_EXTERNAL_HOSTNAME')}{WEBHOOK_PATH}"
+
 bot_manager = BotManager()
 bot = bot_manager.bot
 app = Flask(__name__)
@@ -12,10 +16,6 @@ def setup_webhook():
     bot.set_webhook(url=WEBHOOK_URL)
 
 setup_webhook() # run this code immediately when the program runs
-
-TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_PATH = f"/{TOKEN}"
-WEBHOOK_URL = f"https://{os.getenv('RENDER_EXTERNAL_HOSTNAME')}{WEBHOOK_PATH}"
 
 @app.route("/")
 def home():
@@ -29,4 +29,4 @@ def webhook():
     return "", 200
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=False)
+    app.run(port=5001, debug=False)
