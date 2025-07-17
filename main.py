aa085f38646e21b4afbff5a0bf0e1b93469bc419
Jia Sheng Tay <tayjiasheng@MacBookPro.singtel.com>
2025-07-17 23:05:02 +0800

    add ffmpeg so it is railway compatible

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from bot_manager import BotManager
 from flask import Flask
 from threading import Thread
 import requests, os
+import imageio_ffmpeg
+
+ffmpeg_binary = imageio_ffmpeg.get_ffmpeg_exe()
+os.environ["PATH"] = os.path.dirname(ffmpeg_binary) + os.pathsep + os.environ["PATH"]
 
 bot = BotManager()
 
